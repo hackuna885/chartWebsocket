2ff4faead3230052a796182db5547b4ae00b4409
HACKUNA-MONSTER <oliver885@hotmail.com>
2020-06-14 20:53:06 -0500

    first commit

---
+++ _server.js
@@ -0,0 +1,18 @@
+let Server = require('ws').Server;
+
+let servidor = new Server({port: 5151});
+
+servidor.on('connection', function(conexion){
+    
+    let datos = {contador: 1, contenido: ''};
+
+    conexion.send(JSON.stringify(datos));
+
+    conexion.on('message', function(mensaje) {
+        let objeto = JSON.parse(mensaje);
+
+        objeto.contador = parseInt(objeto.contador) + 1;
+
+        conexion.send(JSON.stringify(objeto));
+    });
+});
